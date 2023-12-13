ac99a9434a4b15d67df950aa876adc5a3163d361
WALKMANxy <artorias555@gmail.com>
2023-12-13 13:12:29 +0100

    Testing apis

--- Day7/build/index.js
+++ Day7/build/index.js
@@ -1,25 +1,42 @@
 "use strict";
-const url = "https://jsonplaceholder.typicode.com/posts";
-/**
- * Executes a function.
- *
- * @param {type} paramName - description of parameter
- * @return {type} description of return value
- */
-const f = (data) => {
-    console.log("Quanto ti viene restituito il dato vai in questa funzione");
-    return data.json();
-    console.timeEnd();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-const f2 = () => {
-    console.log("Invoca quando vai in errore");
-};
-const f3 = (data) => {
-    console.log("data:", data);
-};
-const sample = () => {
-    console.time();
-    const r = fetch(url).then(f3).catch(f2);
-    console.log("Sono qui");
-};
-sample();
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
+const rickAndMortyApiUrl = 'https://rickandmortyapi.com/api/character';
+async function fetchCharacters() {
+    try {
+        const response = await axios_1.default.get(rickAndMortyApiUrl);
+        return response.data.results;
+    }
+    catch (error) {
+        console.error('Error fetching characters:', error.message);
+        return [];
+    }
+}
+function groupCharactersByStatus(characters) {
+    return characters.reduce((grouped, character) => {
+        const { status } = character;
+        grouped[status] = grouped[status] || [];
+        grouped[status].push(character);
+        return grouped;
+    }, {});
+}
+function displayCharactersByStatus(groupedCharacters) {
+    Object.entries(groupedCharacters).forEach(([status, characters]) => {
+        console.log(`${status} Characters:`);
+        characters.forEach(({ name }) => console.log(`  - ${name}`));
+    });
+}
+async function main() {
+    const characters = await fetchCharacters();
+    if (characters.length > 0) {
+        const groupedCharacters = groupCharactersByStatus(characters);
+        displayCharactersByStatus(groupedCharacters);
+    }
+    else {
+        console.log('No characters found.');
+    }
+}
+main();
